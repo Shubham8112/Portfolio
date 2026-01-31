b356399e1a88975f5dc699e330393dca1765f6b5
Shubham Pandey <shubpandeyham@gmail.com>
2026-01-31 10:13:12 +0530

    email regex done✅

--- script.js
+++ script.js
@@ -1,52 +1,3 @@
-console.log("javascript file loaded successfully");
-let num1 = 10;
-let num2 = 5;
-console.log("Sum:", num1 + num2);
-//conditional example
-if(num1 > num2){
-    console.log(`${num1} is greater than ${num2}`);
-}else{
-    console.log(`${num2} is greater than ${num1}`);
-}
-//for loop example
-for(let i = 0;i<num2;i++){
-    console.log(i);
-}
-//array example
-let arr1= [2,4,6,8,10];
-// arr1.forEach(element => {
-//     console.log("Element:", element);
-// });
-console.log(arr1);
-//push example:- push new entry at the end
-arr1.push(12);
-console.log("After push:", arr1);
-//pop example:- remove last entry
-arr1.pop();
-console.log("After pop:", arr1);
-//shift:- remove first entry
-arr1.shift();
-console.log("After shift:", arr1);
-//unshift:- add new entry at the start
-arr1.unshift(0);
-console.log("After unshift:", arr1);
-//object in javascript: key-value pair
-const student = {
-    name: "Ram",
-    age: 21,
-    marks:80
-}
-console.log(`Name: ${student.name},Age: ${student.age}, Marks: ${student.marks}`);
-//
-const students = [
-    {Name: "Shubham", Age:22, Marks:79},
-    {Name: "Ravi" ,Age:20, Marks:85},
-    {Name: "Ankit" ,Age:23, Marks:90}
-];
-for(let i=0;i<students.length;i++){
-    console.log(`Name: ${students[i].Name}, Age: ${students[i].Age}, Marks: ${students[i].Marks}`);
-}
-// ...........
 console.log("JS loaded");
 
 const btn = document.getElementById("darkBtn");
@@ -77,7 +28,7 @@ const name = document.querySelector('#name');
 const email = document.querySelector('#email');
 const msg = document.querySelector('#message');
 const error = document.querySelector('#error');
-const darkbtn = document.querySelector('#darkBtn');
+const emailregex =  /^\S+@\S+\.\S+$/;
 form.addEventListener('submit',(e) =>{
   e.preventDefault();
   error.style.color = ""; 
@@ -91,20 +42,17 @@ form.addEventListener('submit',(e) =>{
   else if(!email.value.trim()){
     // alert('!!!Email is Required!!!');
     error.innerText = '!!!Email is Required!!!';
-  }
-  else if(!msg.value.trim()){
+  }else if(!emailregex.test(email.value.trim())){
+    // alert('Enter valid Email');
+     error.innerText = 'Enter a valid Email!';
+  }else if(!msg.value.trim()){
     // alert('!!!You must add message to send it!!!');
     error.innerText = '!!!You must add message to send it!!!';
-  }else {
+  }
+  else {
   error.style.color = "green";
   error.innerText = "Form submitted successfully!";
   form.reset();
 }
 })
-// const headings = document.querySelectorAll('h3');
 
-// darkbtn.addEventListener('click', () => {
-//   headings.forEach(h3 => {
-//     h3.style.color = '#0f1312';
-//   });
-// });
